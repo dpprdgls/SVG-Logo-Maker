150f494c68f4f71d4f3e2e3f55bd171e852789ea
dpprdgls <74167696+dpprdgls@users.noreply.github.com>
2023-11-11 20:48:08 -0800

    import shapes

---
+++ index.js
@@ -0,0 +1,10 @@
+//import inquirer 
+const inquirer = require('inquirer');
+
+//import file system 
+const fs = require('fs');
+
+//import classes from .lib/shapes 
+const { Triangle, Square, Circle } = require('./lib/shapes.js');
+
+
