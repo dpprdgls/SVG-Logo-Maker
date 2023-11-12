0f6a6487966445d7389dc91b25b6adaa6e825d24
dpprdgls <74167696+dpprdgls@users.noreply.github.com>
2023-11-11 23:08:19 -0800

    add prompts

--- index.js
+++ index.js
@@ -44,3 +44,50 @@ function writeToFile(fileName, answers) {
     });
 }
 
+// This function utilizes inquirer .prompt to prompt the user to answer questions in the command line and save user input
+function promptUser() {
+    inquirer
+      .prompt([
+        // Text prompt
+        {
+          type: "input",
+          message:
+            "What text would you like you logo to display? (Enter up to three characters)",
+          name: "text",
+        },
+        // Text color prompt
+        {
+          type: "input",
+          message:
+            "Choose text color (Enter color keyword OR a hexadecimal number)",
+          name: "textColor",
+        },
+        // Shape choice prompt
+        {
+          type: "list",
+          message: "What shape would you like the logo to render?",
+          choices: ["Triangle", "Square", "Circle"],
+          name: "shape",
+        },
+        // Shape color prompt
+        {
+          type: "input",
+          message:
+            "Choose shapes color (Enter color keyword OR a hexadecimal number)",
+          name: "shapeBgColor",
+        },
+      ])
+      .then((answers) => {
+        // Error handling for text prompt (user must enter 3 characters or less for logo to generate)
+        if (answers.text.length > 3) {
+          console.log("Must enter a value of no more than 3 characters");
+          promptUser();
+        } else {
+          // Calling write file function to generate SVG file
+          writeToFile("logo.svg", answers);
+        }
+      });
+  }
+  
+  // Calling promptUser function so inquirer prompts fire off when application is ran
+  promptUser();
